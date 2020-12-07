cd060ca122f45db9aba4241bdd28ec965c1f8c88
misanaka30 <hanadibuuuu@hotmail.co.jp>
2020-12-07 17:35:12 +0900

    フォーム見た目

--- app/controllers/gosyuins_controller.rb
+++ app/controllers/gosyuins_controller.rb
@@ -22,6 +22,19 @@ class GosyuinsController < ApplicationController
    @gosyuin = Gosyuin.find(params[:id])
   end 
 
+  def edit
+   @gosyuin = Gosyuin.find(params[:id])
+  end
+  
+  def update
+    @gosyuin = Gosyuin.find(params[:id])
+    if @gosyuin.update(gosyuin_params)
+       redirect_to root_path
+    else
+       render :edit
+    end
+  end
+
   private
   def gosyuin_params
    params.require(:gosyuin).permit(:title, :prefecture_id, :stamp_id, :limited_id, :caption, :image ).merge(user_id: current_user.id)
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   #get 'gosyuins/index'
   root to: "gosyuins#index"
-  resources :gosyuins, only: [:new, :create, :index, :show]
+  resources :gosyuins, only: [:new, :create, :index, :show, :edit, :update]
 end
